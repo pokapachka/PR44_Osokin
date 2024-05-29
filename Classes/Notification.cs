3b0adee05c74f7b5d71367ca03a564720fd557f3
pokapachka <pachkaosokin@yandex.ru>
2024-05-29 16:04:05 +0500

    Разработанный классы общего-пользования. Осокин

---
+++ Classes/Notification.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace ПР44_Осокин.Classes
+{
+    public class Notification : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}

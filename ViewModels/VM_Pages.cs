d482a44e6b1d956131c5aa314139a1f90a3baea8
pokapachka <pachkaosokin@yandex.ru>
2024-05-29 16:27:08 +0500

    Разработанная модель представления главного окна. Осокин

---
+++ ViewModels/VM_Pages.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ПР44_Осокин.Classes;
+
+namespace ПР44_Осокин.ViewModels
+{
+    public class VM_Pages : Notification
+    {
+        public VM_Tasks vm_tasks = new VM_Tasks();
+
+        public VM_Pages() => MainWindow.init.frame.Navigate(new View.Main(vm_tasks));
+
+        public RealyCommand OnClose
+        {
+            get
+            {
+                return new RealyCommand(obj =>
+                {
+                    MainWindow.init.Close();
+                });
+            }
+        }
+    }
+}

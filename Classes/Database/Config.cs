2da0ba3486d3a890f08d949ddbdfec7b86a571b2
pokapachka <pachkaosokin@yandex.ru>
2024-05-29 16:00:22 +0500

    Созданный класс Config. Осокин

---
+++ Classes/Database/Config.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ПР44_Осокин.Classes.Database
+{
+    public class Config
+    {
+        public static readonly string connection = "server=localhost;uid=root;database=TaskManager;";
+        public static readonly MySqlServerVersion version = new MySqlServerVersion(new Version(8, 0, 11));
+    }
+}

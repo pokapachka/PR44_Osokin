d2ec79b29f70cb40dd3292f5330a1aba225ae8b5
pokapachka <pachkaosokin@yandex.ru>
2024-05-29 16:23:47 +0500

    Разработанный контекст данных для модели задач. Осокин

---
+++ Context/TasksContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ПР44_Осокин.Context
+{
+    public class TasksContext : DbContext
+    {
+        public DbSet<Models.Tasks> Tasks { get; set; }
+
+        public TasksContext()
+        {
+            Database.EnsureCreated();
+            Tasks.Load();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseMySql(Classes.Database.Config.connection, Classes.Database.Config.version);
+    }
+}
